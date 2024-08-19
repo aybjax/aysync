495e5a3c6ce6f87cc0df5079038a134d8e965899
aybjax <aybjax@gmail.com>
2024-08-19 12:57:19 +0500

    for loop map

--- async/func.go
+++ async/func.go
@@ -42,6 +42,11 @@ type ForIter[T any] struct {
 	Data  T
 }
 
+type ForIterMap[T comparable, U any] struct {
+	Key  T
+	Data U
+}
+
 func For[T any](_ context.Context, s []T) <-chan ForIter[T] {
 	ch := make(chan ForIter[T])
 
@@ -51,6 +56,51 @@ func For[T any](_ context.Context, s []T) <-chan ForIter[T] {
 				Index: ind,
 				Data:  el,
 			}
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+func ForMap[T comparable, U any](_ context.Context, m map[T]U) <-chan ForIterMap[T, U] {
+	ch := make(chan ForIterMap[T, U])
+
+	go func() {
+		for k, v := range m {
+			ch <- ForIterMap[T, U]{
+				Key:  k,
+				Data: v,
+			}
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+func ForMapKey[T comparable, U any](_ context.Context, m map[T]U) <-chan T {
+	ch := make(chan T)
+
+	go func() {
+		for k := range m {
+			ch <- k
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+func ForMapVal[T comparable, U any](_ context.Context, m map[T]U) <-chan U {
+	ch := make(chan U)
+
+	go func() {
+		for _, v := range m {
+			ch <- v
 		}
 
 		close(ch)
