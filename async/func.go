8d87fbcb3a047cf58a27494b36778b21f9303292
aybjax <aybjax@gmail.com>
2024-08-19 12:49:58 +0500

    for loop

--- async/func.go
+++ async/func.go
@@ -1,7 +1,60 @@
 package async
 
-import "runtime"
+import (
+	"context"
+	"runtime"
+)
 
-func Await() {
+func Await(_ context.Context) {
 	runtime.Gosched()
+}
+
+func ForItem[T any](_ context.Context, s []T) <-chan T {
+	ch := make(chan T)
+
+	go func() {
+		for _, el := range s {
+			ch <- el
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+func ForIndex[T any](_ context.Context, s []T) <-chan int {
+	ch := make(chan int)
+
+	go func() {
+		for ind := range s {
+			ch <- ind
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+type ForIter[T any] struct {
+	Index int
+	Data  T
+}
+
+func For[T any](_ context.Context, s []T) <-chan ForIter[T] {
+	ch := make(chan ForIter[T])
+
+	go func() {
+		for ind, el := range s {
+			ch <- ForIter[T]{
+				Index: ind,
+				Data:  el,
+			}
+		}
+
+		close(ch)
+	}()
+
+	return ch
 }
