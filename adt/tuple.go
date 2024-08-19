d0a2bfec1a782f7fcf8e279062ef3b55cbab23f6
aybjax <aybjax@gmail.com>
2024-08-19 11:26:59 +0500

    add tuple

---
+++ adt/tuple.go
@@ -0,0 +1,17 @@
+package adt
+
+type Tuple2[T1, T2 any] struct {
+	Data1 T1
+	Data2 T2
+}
+type Tuple3[T1, T2, T3 any] struct {
+	Data1 T1
+	Data2 T2
+	Data3 T3
+}
+type Tuple4[T1, T2, T3, T4 any] struct {
+	Data1 T1
+	Data2 T2
+	Data3 T3
+	Data4 T4
+}
